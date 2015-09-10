8661a27596540a4d5004158edb3a4a12056f3caa
Jerome Angeles <raizumaraiz@gmail.com>
2015-09-11 03:23:37 +0800

    Insect Bite Minigame

---
+++ Assets/Scripts/BiteScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// For insect bite
+/// </summary>
+public class BiteScript : MonoBehaviour {
+
+	public bool isCreamed = false;
+
+	void OnTriggerStay2D(Collider2D other){
+		if(other.gameObject.tag == "Cream"){
+			isCreamed = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.gameObject.tag == "Cream"){
+			isCreamed = false;
+		}
+	}
+}
---
+++ Assets/Scripts/InsectBiteMinigame.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+public class InsectBiteMinigame : MiniGame {
+
+	public GameObject[] bites;
+	public ArrayList creams;
+
+	/// <summary>
+	/// The prefab
+	/// </summary>
+	public GameObject cream;
+	public TapGesture creamContainer;
+	public GameObject creamStartPosition;
+
+	public bool AllBitesTouchedByCreams(){
+		foreach (GameObject bite in bites){
+			if(!bite.GetComponent<BiteScript>().isCreamed){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update();
+		if(AllBitesTouchedByCreams()){
+			SetUpWin();
+		}
+		else if (GetBarPercentage() < 0f){
+			SetUpLose();
+		}
+	}
+
+	void OnEnable(){
+		creamContainer.Tapped += OnDoubleTap;
+	}
+
+	void OnDisable(){
+		foreach(GameObject g in GameObject.FindGameObjectsWithTag ("Cream")){
+			DestroyImmediate(g);
+		}
+		creamContainer.Tapped -= OnDoubleTap;
+	}
+
+	void OnDoubleTap(object sender, System.EventArgs args){
+		Instantiate(cream, creamStartPosition.transform.position, Quaternion.identity);
+	}
+}
